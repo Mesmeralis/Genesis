4bec0ac02bc212b0e3a610e109fd40b3b43aef2d
zerqzmc@gmail.com <zerqzmc@gmail.com>
2024-03-25 11:07:37 -0400

    Fixed issue with server crashing upon move

--- src/main/java/org/u410/genesis/listeners/GameWaterListener.java
+++ src/main/java/org/u410/genesis/listeners/GameWaterListener.java
@@ -24,14 +24,12 @@ public class GameWaterListener implements Listener {
         Player player = event.getPlayer();
         if(manager.gameRunning && manager.gameName.equalsIgnoreCase("water")) {
             if(manager.inGame.contains(player)) {
-                while(manager.gameRunning) {
                     if(player.getLocation().getBlock().getType() == Material.WATER) {
                         this.manager.winner = player;
                         Bukkit.getServer().broadcastMessage(ColourUtils.colour(this.genesis.genesisPrefix() + " &eThe &d" + this.manager.gameName + "&e game has ended."));
                         Bukkit.getServer().broadcastMessage(ColourUtils.colour(this.genesis.genesisPrefix() + " &eThe winner is &a" + this.manager.winner.getName() + "&e!"));
                         this.manager.gameRunning = false;
                     }
-                }
             }
         }
     }
