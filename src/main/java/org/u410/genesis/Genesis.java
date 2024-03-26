76b30b350c412dff9bc3d720a91b839d0d77cef3
zerqzmc@gmail.com <zerqzmc@gmail.com>
2024-03-25 20:14:25 -0400

    Removed GroupManager dependencies

--- src/main/java/org/u410/genesis/Genesis.java
+++ src/main/java/org/u410/genesis/Genesis.java
@@ -1,27 +1,17 @@
 package org.u410.genesis;
 
 import net.milkbowl.vault.economy.Economy;
-import org.anjocaido.groupmanager.GroupManager;
-import org.anjocaido.groupmanager.dataholder.OverloadedWorldHolder;
-import org.anjocaido.groupmanager.permissions.AnjoPermissionsHandler;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.u410.genesis.commands.*;
-import org.u410.genesis.listeners.ChatListener;
 import org.u410.genesis.listeners.GameWaterListener;
 import org.u410.genesis.listeners.JoinQuitListener;
 import org.u410.genesis.managers.GameManager;
 
-import java.util.Arrays;
-import java.util.List;
 
 public final class Genesis extends JavaPlugin {
 
-    public GroupManager groupManager;
     public GameManager gameManager = new GameManager(this);
     private static Economy econ;
     @Override
@@ -52,7 +42,6 @@ public final class Genesis extends JavaPlugin {
     }
     public void registerEvents() {
         Bukkit.getPluginManager().registerEvents(new JoinQuitListener(this), this);
-        Bukkit.getPluginManager().registerEvents(new ChatListener(this), this);
         Bukkit.getPluginManager().registerEvents(new GameWaterListener(this, this.gameManager), this);
     }
 
@@ -60,65 +49,6 @@ public final class Genesis extends JavaPlugin {
         return this.getConfig().getString("genesisPrefix");
     }
 
-    public boolean hasGroupManager() {
-
-        if (groupManager != null) return true;
-
-        final PluginManager pluginManager = this.getServer().getPluginManager();
-        final Plugin GMplugin = pluginManager.getPlugin("GroupManager");
-
-        if (GMplugin != null && GMplugin.isEnabled()) {
-            groupManager = (GroupManager)GMplugin;
-            return true;
-        }
-        return false;
-    }
-
-    public String getGroup(final Player player) {
-        if (!hasGroupManager()) return null;
-
-        final AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(player);
-        if (handler == null) return null;
-
-        return handler.getGroup(player.getName());
-    }
-
-    public boolean setGroup(final Player player, final String group) {
-        if (!hasGroupManager()) return false;
-
-        final OverloadedWorldHolder handler = groupManager.getWorldsHolder().getWorldData(player);
-        if (handler == null) return false;
-
-        handler.getUser(player.getName()).setGroup(handler.getGroup(group));
-        return true;
-    }
-
-    public List<String> getGroups(final Player player) {
-        if (!hasGroupManager()) return null;
-
-        final AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(player);
-        if (handler == null) return null;
-
-        return Arrays.asList(handler.getGroups(player.getName()));
-    }
-
-    public String getPrefix(final Player player) {
-        if (!hasGroupManager()) return null;
-
-        final AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(player);
-        if (handler == null) return null;
-
-        return handler.getUserPrefix(player.getName());
-    }
-
-    public String getSuffix(final Player player) {
-        if (!hasGroupManager()) return null;
-
-        final AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(player);
-        if (handler == null) return null;
-
-        return handler.getUserSuffix(player.getName());
-    }
 
     private void setupEconomy() {
         if (getServer().getPluginManager().getPlugin("Vault") == null) {
